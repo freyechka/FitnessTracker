2e183749dc410a2640aa55a22768e371e70ab1a6
freyyyka <chicherov.da@dvfu.ru>
2025-01-21 21:35:04 +1000

    a bit optimized

--- app/src/main/java/com/example/fitnesstracker/model/ActivityRepository.kt
+++ app/src/main/java/com/example/fitnesstracker/model/ActivityRepository.kt
@@ -8,35 +8,40 @@ import java.time.ZoneId
 class ActivityRepository(private val activityDao: ActivityDao) {
 
     fun getMyActivities(): Flow<List<ActivityListItem>> {
-        return activityDao.getMyActivities()
-            .map { items ->
-                mutableListOf<ActivityListItem>().apply {
-                    val sortedItems = items.sortedByDescending { it.end }
-                    sortedItems.groupBy { Instant.ofEpochMilli(it.end).atZone(ZoneId.systemDefault()).toLocalDate() }.forEach { (date, groupItems) ->
-                        add(ActivityListItem.DataHeader(date))
-                        groupItems.forEach { item ->
-                            add(ActivityListItem.ActivityItem(item))
-                        }
-                    }
-                }
-            }
+        return mapActivitiesToListItems(activityDao.getMyActivities())
+
     }
 
     fun getActivities(): Flow<List<ActivityListItem>> {
-        return activityDao.getActivities().map { items ->
-            mutableListOf<ActivityListItem>().apply {
-                val sortedItems = items.sortedByDescending { it.end }
-                sortedItems.groupBy { Instant.ofEpochMilli(it.end).atZone(ZoneId.systemDefault()).toLocalDate() }.forEach { (date, groupItems) ->
-                    add(ActivityListItem.DataHeader(date))
-                    groupItems.forEach { item ->
-                        add(ActivityListItem.ActivityItem(item))
-                    }
-                }
-            }
-        }
+        return mapActivitiesToListItems(activityDao.getActivities())
     }
 
     suspend fun addActivity(activity: Activity) {
         activityDao.addActivity(activity)
+    }
+}
+
+//private fun mapActivitiesToListItems(activities: Flow<List<Activity>>) : Flow<List<ActivityListItem>> {
+//    return activities.map { items ->
+//        mutableListOf<ActivityListItem>().apply {
+//            val sortedItems = items.sortedByDescending { it.end }
+//            sortedItems.groupBy { Instant.ofEpochMilli(it.end).atZone(ZoneId.systemDefault()).toLocalDate() }.forEach { (date, groupItems) ->
+//                add(ActivityListItem.DataHeader(date))
+//                groupItems.forEach { item ->
+//                    add(ActivityListItem.ActivityItem(item))
+//                }
+//            }
+//        }
+//    }
+//}
+
+private fun mapActivitiesToListItems(activities: Flow<List<Activity>>) : Flow<List<ActivityListItem>> {
+    return activities.map { items ->
+        items.sortedByDescending { it.end }
+            .groupBy { Instant.ofEpochMilli(it.end).atZone(ZoneId.systemDefault()).toLocalDate() }
+            .flatMap { (date, groupItems) ->
+                        listOf(ActivityListItem.DataHeader(date)) +
+                        groupItems.map { item -> ActivityListItem.ActivityItem(item) }
+            }
     }
 }
